8c3dbeaa3a8fa55e1f01e7f3fb3f9055468697e5
Pyristix <61607183+Pyristix@users.noreply.github.com>
2020-02-29 03:18:37 -0600

    Shuttlecock Mk 1

--- game1.js
+++ game1.js
@@ -1,99 +1,249 @@
 var canvas = document.getElementById("myCanvas")
 var ctx = canvas.getContext("2d");
 
-var x = canvas.width/2;
-var y = canvas.height-30;
+let fps, fpsInterval, startTime, now, then, elapsed;
+fps = 60;
+fpsInterval = 1000 / fps;
 
-var dx = 2;
-var dy = -2;
+//Handles images
+var leg = new Image();
+var shuttleCock = new Image();
+var legLoaded = false;
+var shuttleCockLoaded = false;
+leg.src = "Spells.png";
+shuttleCock.src = "SunsetFromACliff.png";
 
-var ballRadius = 10;
+//Listeners
+leg.addEventListener('load', function() {legLoaded = true;}, false);
+shuttleCock.addEventListener('load', function() {shuttleCockLoaded = true;}, false);
+document.addEventListener("click", function(){clickDetected();});
+document.addEventListener('touchstart', function(e){clickDetected();}, false)
 
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth) / 2;
+var pressedPlay = false;
 
-var rightPressed = false;
-var leftPressed = false;
+//Gameplay variables
+var groundY = 2300;
+var legY = 1500;
+var leg_dY = 0;
+var shuttleCockY = 50;
+var shuttleCock_dY = 1;
+var alreadyClicked = false;
+var alreadyHitBall = false;
+var gameOver = false;
+var startingPlay = false;
+var tapToPlayScreenUp = true;
+var score = 0;
 
 
-function drawBall() {
-ctx.beginPath();
-ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-ctx.fillStyle = "#0095DD";
-ctx.fill();
-ctx.closePath();
-}
 
-function drawPaddle() {
-    ctx.beginPath();
-    ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
+
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	
+	//Creates loading message when images aren't loaded yet
+	if (!legLoaded || !shuttleCockLoaded){
+		drawLoadingMessage();
+		return;
+	}
+	
+	//Draws leg and ball
+	drawLeg();
 	drawBall();
-	drawPaddle();
 	
-	x += dx;
-	y += dy;
+	if(tapToPlayScreenUp){
+		drawTapToStart();
+		return;
+	}
 	
-	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-		dx = -dx;
-	}
-
-	if(y + dy < ballRadius) {
-		dy = -dy;
-	}else if(y + dy > canvas.height-ballRadius) {
-		if(x > paddleX && x < paddleX + paddleWidth) {
-			dy = -dy;
-		}
-		else {
-			alert("GAME OVER");
-			document.location.reload();
-			clearInterval(interval);
-		}
-	}
-}
+	//Handles shuttlecock gravity
+	shuttleCockY += shuttleCock_dY;
 	
-	if(rightPressed) {
-		paddleX += 7;
-		if (paddleX + paddleWidth > canvas.width){
-			paddleX = canvas.width - paddleWidth;
-		}
+	//Handles shuttlecock bouncing
+	if(shuttleCockY+200 < groundY-50 && shuttleCockY+200 > groundY-250 && alreadyClicked && !alreadyHitBall){
+		shuttleCock_dY = Math.floor(Math.random() * -30) - 30;
+		score++;
+		alreadyHitBall = true;
+		console.log("score is " + score);
+		
 	}
-	else if(leftPressed) {
-		paddleX -= 7;
-		if (paddleX < 0){
-			paddleX = 0;
-		}
-	}
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
+	shuttleCock_dY += 1;
 	
-}
+	//Handles what happens when the shuttlecock falls to the floor and restarting the game
+	if(shuttleCockY + 200 + shuttleCock_dY >= groundY){
+		shuttleCockY = groundY-200;
+		shuttleCock_dY = 0;
+		
+		gameOver = true;
+		
+		//Handles restarting the game
+	}
+	
+	//Handles the game over screen
+	if(gameOver){
+		drawGameOverScreen()
+	}
+	
 
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
+	//Handles leg falling back down after jumping up
+	if(legY+800 + leg_dY <= groundY){
+		leg_dY += 4;
+	}else{
+		legY = groundY-800;
+		leg_dY = 0;
+		alreadyClicked = false;
+		alreadyHitBall = false;
+	}
+	legY += leg_dY;
+	
+	/*if(Math.abs(legY+600 - shuttleCockY) < 50){
+		shuttleCock_dY = Math.floor(Math.random() * - 50) - 35;
+	}*/
+	
+	//ctx.drawImage(leg, 0, legY);
+	//ctx.drawImage(shuttleCock, 0, shuttleCockY);
+	
 }
 
-var interval = setInterval(draw, 10);
+function drawBall(){
+	ctx.beginPath();
+	ctx.rect(700,shuttleCockY,200,200);
+	ctx.fillStyle = "red";
+	ctx.fill();
+	ctx.closePath();
+}
+
+function drawLeg(){
+	ctx.beginPath();
+	ctx.rect(700,legY,200,800);
+	ctx.fillStyle = "black";
+	ctx.fill();
+	ctx.closePath();
+	
+	ctx.beginPath();
+	ctx.rect(700,legY+600,200,200);
+	ctx.fillStyle = "blue";
+	ctx.fill();	
+	ctx.closePath();
+}
+
+function clickDetected(){
+	//If the leg is already up in the air, then don't do anything when the user clicks
+	if(alreadyClicked){
+		return;
+	}
+	
+	if(tapToPlayScreenUp){
+		tapToPlayScreenUp = false;
+		return;
+	}
+	
+	if(gameOver){
+		legY = 1500;
+		leg_dY = 0;
+		shuttleCockY = 50;
+		shuttleCock_dY = 1;
+		alreadyClicked = false;
+		alreadyHitBall = false;
+		gameOver = false;
+		//pressedPlay = false;
+		score = 0;
+		
+		tapToPlayScreenUp = true;
+		return;
+	}
+	/*if(startingPlay){
+		startingPlay = false;
+		return;
+	}*/
+	
+	
+	leg_dY -= 55;
+	alreadyClicked = true;
+}
+
+function drawTapToStart(){
+	ctx.font = "64px avenir";
+	ctx.fillStyle = "black";
+	ctx.fillText("Tap anywhere to start!", canvas.width/8, canvas.height/2);
+}
+
+function drawGameOverScreen(){
+	ctx.font = "128px avenir";
+	ctx.fillStyle = "black";
+	ctx.fillText("GAME OVER", canvas.width/5, canvas.height/4);
+	ctx.font = "128px avenir";
+	ctx.fillText("SCORE: " + score, canvas.width/4, canvas.height/3);
+	ctx.font = "64px avenir";
+	ctx.fillText("TAP ANYWHERE TO CONTINUE", canvas.width/8, canvas.height/2);
+}
+
+function drawLoadingMessage() {
+	ctx.font = "32px Arial";
+	ctx.fillStyle = "black";
+	ctx.fillText("LOADING...", canvas.width/2, canvas.height/2);
+}
+
+function startAnimating(fps) {
+	then = Date.now();
+	startTime = then;
+	animate();
+}
+
+
+function animate() {
+	requestAnimationFrame(animate);
+
+	now = Date.now();
+	elapsed = now - then;
+
+	if (elapsed > fpsInterval) {
+
+		// Get ready for next frame by setting then=now, but also adjust for your
+		// specified fpsInterval not being a multiple of RAF's interval (16.7ms)
+		then = now - (elapsed % fpsInterval);
+
+
+		draw();
+	}
+}
+
+startAnimating(fps);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
