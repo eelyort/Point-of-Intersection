46af85bec71503d99530bd3cbd398665736ef7d5
Pyristix <61607183+Pyristix@users.noreply.github.com>
2020-02-28 20:52:33 -0600

    Update game1.js

--- game1.js
+++ game1.js
@@ -45,9 +45,19 @@ function draw() {
 		dx = -dx;
 	}
 
-	if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+	if(y + dy < ballRadius) {
 		dy = -dy;
+	}else if(y + dy > canvas.height-ballRadius) {
+		if(x > paddleX && x < paddleX + paddleWidth) {
+			dy = -dy;
+		}
+		else {
+			alert("GAME OVER");
+			document.location.reload();
+			clearInterval(interval);
+		}
 	}
+}
 	
 	if(rightPressed) {
 		paddleX += 7;
